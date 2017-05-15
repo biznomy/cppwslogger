c8674a60fb758d303e15fde4436a38e8dad82474
im-komo <pravesh.technolabs@gmail.com>
2017-05-15 17:50:00 +0530

    running solution websocket using tail command
    Drawback limitation of live reporting and chances of missing logs

--- CloudServer.cpp
+++ CloudServer.cpp
@@ -19,7 +19,6 @@
 // SPDX-License-Identifier:	BSL-1.0
 //
 
-
 #include "Poco/Net/HTTPServer.h"
 #include "Poco/Net/HTTPRequestHandler.h"
 #include "Poco/Net/HTTPRequestHandlerFactory.h"
@@ -36,9 +35,14 @@
 #include "Poco/Util/HelpFormatter.h"
 #include "Poco/Format.h"
 #include <iostream>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+
 #define SAMPLE_INTERVAL  (1 * 1000 * 1000)
 
-
+using namespace std;
 using Poco::Net::ServerSocket;
 using Poco::Net::WebSocket;
 using Poco::Net::WebSocketException;
@@ -57,14 +61,13 @@ using Poco::Util::Option;
 using Poco::Util::OptionSet;
 using Poco::Util::HelpFormatter;
 
-
 class PageRequestHandler: public HTTPRequestHandler
-	/// Return a HTML document with some JavaScript creating
-	/// a WebSocket connection.
+/// Return a HTML document with some JavaScript creating
+/// a WebSocket connection.
 {
 public:
-	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
-	{
+	void handleRequest(HTTPServerRequest& request,
+			HTTPServerResponse& response) {
 		response.setChunkedTransferEncoding(true);
 		response.setContentType("text/html");
 		std::ostream& ostr = response.send();
@@ -76,7 +79,8 @@ public:
 		ostr << "{";
 		ostr << "  if (\"WebSocket\" in window)";
 		ostr << "  {";
-		ostr << "    var ws = new WebSocket(\"ws://" << request.serverAddress().toString() << "/ws\");";
+		ostr << "    var ws = new WebSocket(\"ws://"
+				<< request.serverAddress().toString() << "/ws\");";
 		ostr << "    ws.onopen = function()";
 		ostr << "      {";
 		ostr << "        ws.send(\"Hello, world!\");";
@@ -101,22 +105,41 @@ public:
 		ostr << "</head>";
 		ostr << "<body>";
 		ostr << "  <h1>WebSocket Server</h1>";
-		ostr << "  <p><a href=\"javascript:WebSocketTest()\">Run WebSocket Script</a></p>";
+		ostr
+				<< "  <p><a href=\"javascript:WebSocketTest()\">Run WebSocket Script</a></p>";
 		ostr << "</body>";
 		ostr << "</html>";
 	}
 };
 
-
 class WebSocketRequestHandler: public HTTPRequestHandler
-	/// Handle a WebSocket connection.
+/// Handle a WebSocket connection.
 {
 public:
-	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
-	{
+
+	std::string exec(const char* cmd) {
+		char buffer[128];
+		std::string result = "";
+		FILE* pipe = popen(cmd, "r");
+		if (!pipe)
+			throw std::runtime_error("popen() failed!");
+		try {
+			while (!feof(pipe)) {
+				if (fgets(buffer, 128, pipe) != NULL)
+					result += buffer;
+			}
+		} catch (...) {
+			pclose(pipe);
+			throw;
+		}
+		pclose(pipe);
+		return result;
+	}
+
+	void handleRequest(HTTPServerRequest& request,
+			HTTPServerResponse& response) {
 		Application& app = Application::instance();
-		try
-		{
+		try {
 			WebSocket ws(request, response);
 			app.logger().information("WebSocket connection established.");
 			char sndBuffer[4048];
@@ -128,28 +151,25 @@ public:
 			n = ws.receiveFrame(buffer, sizeof(buffer), flags);
 			std::string str(buffer);
 			std::cout << str << std::endl;
-			do
-			{
+			do {
 //				Poco::Timestamp now;
 //				long time_value = now.epochTime() * 1000;
 
-
-				std::string result = "some text";
+				std::string result = exec("tail  /home/ubuntu/error.log");
 				strcpy(sndBuffer, result.c_str());
 
 				ws.sendFrame(sndBuffer, sizeof(sndBuffer), flags);
-				usleep (SAMPLE_INTERVAL);
-			}
-			while (n > 0 && (flags & WebSocket::FRAME_OP_BITMASK) != WebSocket::FRAME_OP_CLOSE);
+				usleep(SAMPLE_INTERVAL);
+			} while (n > 0
+					&& (flags & WebSocket::FRAME_OP_BITMASK)
+							!= WebSocket::FRAME_OP_CLOSE);
 			app.logger().information("WebSocket connection closed.");
-		}
-		catch (WebSocketException& exc)
-		{
+		} catch (WebSocketException& exc) {
 			app.logger().log(exc);
-			switch (exc.code())
-			{
+			switch (exc.code()) {
 			case WebSocket::WS_ERR_HANDSHAKE_UNSUPPORTED_VERSION:
-				response.set("Sec-WebSocket-Version", WebSocket::WEBSOCKET_VERSION);
+				response.set("Sec-WebSocket-Version",
+						WebSocket::WEBSOCKET_VERSION);
 				// fallthrough
 			case WebSocket::WS_ERR_NO_HANDSHAKE:
 			case WebSocket::WS_ERR_HANDSHAKE_NO_VERSION:
@@ -163,101 +183,87 @@ public:
 	}
 };
 
-
-class RequestHandlerFactory: public HTTPRequestHandlerFactory
-{
+class RequestHandlerFactory: public HTTPRequestHandlerFactory {
 public:
-	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request)
-	{
+	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request) {
 		Application& app = Application::instance();
-		app.logger().information("Request from "
-			+ request.clientAddress().toString()
-			+ ": "
-			+ request.getMethod()
-			+ " "
-			+ request.getURI()
-			+ " "
-			+ request.getVersion());
+		app.logger().information(
+				"Request from " + request.clientAddress().toString() + ": "
+						+ request.getMethod() + " " + request.getURI() + " "
+						+ request.getVersion());
 
-		for (HTTPServerRequest::ConstIterator it = request.begin(); it != request.end(); ++it)
-		{
+		for (HTTPServerRequest::ConstIterator it = request.begin();
+				it != request.end(); ++it) {
 			app.logger().information(it->first + ": " + it->second);
 		}
 
-		if(request.find("Upgrade") != request.end() && Poco::icompare(request["Upgrade"], "websocket") == 0)
+		if (request.find("Upgrade") != request.end()
+				&& Poco::icompare(request["Upgrade"], "websocket") == 0)
 			return new WebSocketRequestHandler;
 		else
 			return new PageRequestHandler;
 	}
 };
 
-
 class WebSocketServer: public Poco::Util::ServerApplication
-	/// To test the WebSocketServer you can use any web browser (http://localhost:9980/).
+/// To test the WebSocketServer you can use any web browser (http://localhost:9980/).
 {
 public:
-	WebSocketServer(): _helpRequested(false)
-	{
+	WebSocketServer() :
+			_helpRequested(false) {
 	}
 
-	~WebSocketServer()
-	{
+	~WebSocketServer() {
 	}
 
 protected:
-	void initialize(Application& self)
-	{
+	void initialize(Application& self) {
 		loadConfiguration(); // load default configuration files, if present
 		ServerApplication::initialize(self);
 	}
 
-	void uninitialize()
-	{
+	void uninitialize() {
 		ServerApplication::uninitialize();
 	}
 
-	void defineOptions(OptionSet& options)
-	{
+	void defineOptions(OptionSet& options) {
 		ServerApplication::defineOptions(options);
 
 		options.addOption(
-			Option("help", "h", "display help information on command line arguments")
-				.required(false)
-				.repeatable(false));
+				Option("help", "h",
+						"display help information on command line arguments").required(
+						false).repeatable(false));
 	}
 
-	void handleOption(const std::string& name, const std::string& value)
-	{
+	void handleOption(const std::string& name, const std::string& value) {
 		ServerApplication::handleOption(name, value);
 
 		if (name == "help")
 			_helpRequested = true;
 	}
 
-	void displayHelp()
-	{
+	void displayHelp() {
 		HelpFormatter helpFormatter(options());
 		helpFormatter.setCommand(commandName());
 		helpFormatter.setUsage("OPTIONS");
-		helpFormatter.setHeader("A sample HTTP server supporting the WebSocket protocol.");
+		helpFormatter.setHeader(
+				"A sample HTTP server supporting the WebSocket protocol.");
 		helpFormatter.format(std::cout);
 	}
 
-	int main(const std::vector<std::string>& args)
-	{
-		if (_helpRequested)
-		{
+	int main(const std::vector<std::string>& args) {
+		if (_helpRequested) {
 			displayHelp();
-		}
-		else
-		{
+		} else {
 			// get parameters from configuration file
-			unsigned short port = (unsigned short) config().getInt("WebSocketServer.port", 9980);
+			unsigned short port = (unsigned short) config().getInt(
+					"WebSocketServer.port", 9980);
 
 			// set-up a server socket
 			ServerSocket svs(port);
 			// set-up a HTTPServer instance
-			HTTPServer srv(new RequestHandlerFactory, svs, new HTTPServerParams);
+			HTTPServer srv(new RequestHandlerFactory, svs,
+					new HTTPServerParams);
 			// start the HTTPServer
 			srv.start();
 			// wait for CTRL-C or kill
@@ -271,6 +277,5 @@ protected:
 private:
 	bool _helpRequested;
 };
-
 
 POCO_SERVER_MAIN(WebSocketServer)
